d347349fa088d3e7ceb2294f56d202545978c3d7
Timothée <timotheeithier@gmail.com>
2025-08-25 11:58:42 +0200

    start

--- src/main/java/net/TitusTheDuck/dwarfcivilisation/DwarfCivilisationMod.java
+++ src/main/java/net/TitusTheDuck/dwarfcivilisation/DwarfCivilisationMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package net.TitusTheDuck.dwarfcivilisation;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,16 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(DwarfCivilisationMod.MOD_ID)
+public class DwarfCivilisationMod
 {
+
+    public static final String MOD_ID = "dwarfcivilisation"
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public DwarfCivilisationMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
