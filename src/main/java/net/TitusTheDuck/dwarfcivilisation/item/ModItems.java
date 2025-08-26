5a5632fa2bb0e39a5287206c9bdee97819c5dda2
Timothée <timotheeithier@gmail.com>
2025-08-26 21:38:20 +0200

    de la bouffe!

--- src/main/java/net/TitusTheDuck/dwarfcivilisation/item/ModItems.java
+++ src/main/java/net/TitusTheDuck/dwarfcivilisation/item/ModItems.java
@@ -1,8 +1,12 @@
 package net.TitusTheDuck.dwarfcivilisation.item;
 
 import net.TitusTheDuck.dwarfcivilisation.DwarfCivilisationMod;
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -34,6 +38,50 @@ public class ModItems {
 
     public static final RegistryObject<Item> RENFORCED_STEEL_INGOTS = ITEMS.register("renforced_steel_ingots",
             () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)));
+
+
+    // bouffe
+
+                            // Laitage
+
+    public static final RegistryObject<Item> CAVERN_CHEESE_SLICE = ITEMS.register("cavern_cheese_slice",
+            () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)
+                    .food(new Food.Builder().hunger(2).saturation(0.5F).build())));
+
+
+    public static final RegistryObject<Item> CAVERN_CHEESE_WHEEL = ITEMS.register("cavern_cheese_wheel",
+            () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)
+                    .food(new Food.Builder().hunger(6).saturation(0.8F).build())));
+
+    public static final RegistryObject<Item> CHEESE_PIE = ITEMS.register("cheese_pie",
+            () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)
+                    .food(new Food.Builder().hunger(8).saturation(1F).build())));
+
+    public static final RegistryObject<Item> CHEESE_SLICE = ITEMS.register("cheese_slice",
+            () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)
+                    .food(new Food.Builder().hunger(2).saturation(0.5F).build())));
+
+    public static final RegistryObject<Item> CHEESE_WHEEL = ITEMS.register("cheese_wheel",
+            () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)
+                    .food(new Food.Builder().hunger(6).saturation(0.8F).build())));
+
+    public static final RegistryObject<Item> FERMENT_GOAT_MILK = ITEMS.register("ferment_goat_milk",
+            () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)
+                    .food(new Food.Builder().hunger(6).saturation(0.8F).build())));
+
+
+
+
+
+
+                            //Liquide
+
+
+    public static final RegistryObject<Item> LIGHTNING_BEER_PINT = ITEMS.register("lightning_beer_pint",
+            () -> new Item(new Item.Properties().group(ModItemGroup.DWARF_CIVILISATION_GROUP)
+                    .food(new Food.Builder().hunger(6).saturation(0.8F).effect(()-> new EffectInstance
+                            (Effects.NIGHT_VISION, 20*30, 0), 1).build())));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
